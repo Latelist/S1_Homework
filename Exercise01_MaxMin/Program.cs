7fc554c9a59f57fa508bb0afe2bdf61bd59fd921
Salatchen <pozhivi.s.moe@gmail.com>
2022-06-20 17:41:26 +0300

    Homework first draft

---
+++ Exercise01_MaxMin/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7, min = 5
+a = 2 b = 10 -> max = 10, min = 2
+a = -9 b = -3 -> max = -3, min = -9 
+*/
+
+Console.WriteLine("Enter first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
+}
---
+++ Exercise03_Even/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ((a % 2) > 0)
+{
+    Console.WriteLine("No, the number isn't even");
+}
+else
+{
+    Console.WriteLine("Yes, the number is even");
+}
